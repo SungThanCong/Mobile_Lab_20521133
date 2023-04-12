3d9347c4fcdf285430995e88432de722992a44e7
Chinh <blueskylove2002@gmail.com>
2023-04-12 15:14:39 +0700

    Khởi tạo lab3, thêm firestore

---
+++ app/src/main/java/com/example/mobile_lab_20521133/MainActivity.java
@@ -0,0 +1,134 @@
+package com.example.mobile_lab_20521133;
+
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.mobile_lab_20521133.Model.Person;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText edtName, edtPhone;
+    private Button btnAddSqlite, btnAddFs, btnQuerySqlite, btnQueryFs;
+    private ListView lsvData;
+    private ArrayList<String> personData;
+    private ArrayAdapter<String> personAdapter;
+    FirebaseFirestore firestore;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        firestore = FirebaseFirestore.getInstance();
+        personData = new ArrayList<>();
+
+        AddControls();
+        AddEvents();
+    }
+    private void AddControls() {
+        edtName = findViewById(R.id.edtName);
+        edtPhone = findViewById(R.id.edtPhone);
+        btnAddSqlite = findViewById(R.id.btnAddSqlite);
+        btnAddFs = findViewById(R.id.btnAddFs);
+        btnQueryFs = findViewById(R.id.btnQueryFs);
+        btnQuerySqlite = findViewById(R.id.btnQuerySqlite);
+        lsvData = findViewById(R.id.lsvData);
+
+        personAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,personData);
+        lsvData.setAdapter(personAdapter);
+    }
+
+    private void AddEvents() {
+        btnAddFs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = edtName.getText().toString();
+                String phone = edtPhone.getText().toString();
+
+                if(name.length() <= 0 && phone.length() <= 0) {
+                    Toast.makeText(getApplicationContext(),"Input không được để trống",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                // Create a new user with a first and last name
+                Map<String, Object> value = new HashMap<>();
+                value.put("name", name);
+                value.put("phone",phone);
+
+                // Add a new document with a generated ID
+                firestore.collection("person")
+                        .add(value)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                                Toast.makeText(getApplicationContext(),"Thêm dữ liệu thành công",Toast.LENGTH_LONG).show();
+                                edtName.setText("");
+                                edtPhone.setText("");
+
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w(TAG, "Error adding document", e);
+                                Toast.makeText(getApplicationContext(),"Thêm dữ liệu thất bại",Toast.LENGTH_LONG).show();
+
+                            }
+                        });
+
+            }
+        });
+
+        btnQueryFs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                personData.clear();
+                firestore.collection("person")
+                    .get()
+                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if (task.isSuccessful()) {
+                                for (QueryDocumentSnapshot document : task.getResult()) {
+                                    personData.add("Name: " +document.get("name").toString() + "\nPhone: "+ document.get("phone").toString());
+                                }
+                                personAdapter.notifyDataSetChanged();
+                                Toast.makeText(getApplicationContext(), "Lấy dữ liệu thành công", Toast.LENGTH_LONG).show();
+
+                            } else {
+                                Log.w(TAG, "Error query document");
+                                Toast.makeText(getApplicationContext(), "Lấy dữ liệu thất bại", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
+                }
+            });
+
+
+    }
+
+    
+}
---
+++ app/src/main/java/com/example/mobile_lab_20521133/Model/Person.java
@@ -0,0 +1,31 @@
+package com.example.mobile_lab_20521133.Model;
+
+public class Person {
+    private String fullName;
+    private String phoneNumber;
+
+    public Person(){
+
+    }
+
+    public Person(String fullName, String phoneNumber) {
+        this.fullName = fullName;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
